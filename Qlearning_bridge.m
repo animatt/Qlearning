c91c147a2d59f1275490e5d1075debe8e2ee80ad
King Crawford <crawboy@me.com>
2017-03-26 21:05:37 -0400

    modify code to work with new image

--- Qlearning_bridge.m
+++ Qlearning_bridge.m
@@ -10,7 +10,7 @@ Im = imresize(rgb2gray(Im), [32 24]);
 GR = imquantize(Im, multithresh(Im, 1));
 [m, n] = size(GR);
 
-start = sub2ind(size(GR), 32 * ones(1, 5), 6 : 10);
+start = sub2ind(size(GR), 31 * ones(1, 5), 6 : 10);
 finish = sub2ind(size(GR), 2 * ones(1, 5), 15 : 19);
 
 GR([start; finish]) = 1.5;
@@ -20,32 +20,31 @@ imagesc(GR), colormap(gray), axis equal
 % initialize agent (row, col[, action])
 Q = zeros(m, n, 4);
 target_policy = ones(m, n);
+Action_Set = [1 0; 0 -1; -1 0; 0 1]';
 alpha = 0.1;
 gamma = 0.95;
 
 learner_converging = true;
 while learner_converging
     S = datasample(start, 1);
-    action = randi(4);
-    
-    % get subscripts
-    rstep = -round(sin((action - 1) * pi / 2));
-    cstep = round(cos((action - 1) * pi / 2));
-    
-    [row, col] = ind2sub([m n], S);
-    
-    rnext = bound(row + rstep);
-    cnext = bound(col + cnext);
-    SA = sub2ind(size(Q), row, col, action);
+    A = randi(4);
+    step = Action_Set * (1 : 4 == A)';
     
     reward = 0;
+    
+    % get subscripts
+    [row, col] = ind2sub([m n], S);
+    SA = sub2ind(size(Q), row, col, A);
+    
+    [~, S2] = bound(GR, [row; col], step + [row; col]);
+    
     % do initial backup
-    Q(SA) = Q(SA) + alpha * (reward + max(Q(rnext, cnext, :)));
+    Q(SA) = Q(SA) + alpha * (reward + max(Q(S2(1), S2(2), :)));
     
     episode_in_progress = true;
     while episode_in_progress
-        action = randi(4);
-        rstep = -round(sin((action - 1) * pi / 2));
-        cstep = round(cos((action - 1) * pi / 2));
+        A = randi(4);
+        rstep = -round(sin((A - 1) * pi / 2));
+        cstep = round(cos((A - 1) * pi / 2));
     end
 end
