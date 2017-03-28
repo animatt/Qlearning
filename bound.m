e8a0dcaed2c6c244ca51d9d23a390b87233b6609
King Crawford <crawboy@me.com>
2017-03-28 07:17:45 -0400

    bound returns ~state_not_terminal only at the finish line

--- bound.m
+++ bound.m
@@ -2,10 +2,10 @@ function [state_not_terminal, resultant_position] = bound(image, pos, pnext)
 % check the next state and ensure it is allowed before accepting it. If it
 % is not permitted, return the previous state. If the next state is on the
 % finish line, permit it note that the state is terminal.
-if image(pnext(1), pnext(2)) == 1.5 % reached finish line
+if image(pnext(1), pnext(2)) == 1.5 && pos(1) < 29% reached finish line
     resultant_position = pnext;
     state_not_terminal = false;
-elseif GR(pnext(1), pnext(2)) == 2
+elseif image(pnext(1), pnext(2)) == 2
     resultant_position = pnext;
     state_not_terminal = true;
 else
